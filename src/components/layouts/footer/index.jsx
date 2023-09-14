280694a96e3e8a2f81ecbb17a1d118cf4badb8bf
Anonymous Developer <you@example.com>
2023-09-14 23:37:01 +0500

    change footer

--- src/components/layouts/footer/index.jsx
+++ src/components/layouts/footer/index.jsx
@@ -6,7 +6,41 @@ const index = memo(() => {
       <div className="footer__container">
         <div className="container">
           <div className="row">
-            
+            <div className="col-6 col-md-2">
+              <h5 className="footer_title">Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">About</a></li>
+              </ul>
+            </div>
+
+            <div className="col-6 col-md-2">
+              <h5 className="footer_title">Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">About</a></li>
+              </ul>
+            </div>
+
+            <div className="col-6 col-md-2">
+              <h5 className="footer_title">Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">About</a></li>
+              </ul>
+            </div>
+            <div className="col-md-5 float-end offset-md-1">
+                <h5 className="footer_title">Axborot Xizmati</h5>
+            </div>
           </div>
         </div>
       </div>
