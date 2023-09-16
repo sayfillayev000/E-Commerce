71d98e1820615f0771c561a44e4cc1267257ca29
sayfillayev000 <muhammadsayfillayev1027@gmail.com>
2023-09-16 14:53:00 +0500

    a

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import React, { memo } from "react";
 const index = memo(() => {
   return (
     <>
-      <h1 className="text-bg-success">Home</h1>
+      <h1 className="">Home</h1>
     </>
   );
 });
