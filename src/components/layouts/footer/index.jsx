2e9a2e4c0b152db9917105cd972fb9a24698fbd3
Anonymous Developer <you@example.com>
2023-09-16 17:17:27 +0500

    delete transition

--- src/components/layouts/footer/index.jsx
+++ src/components/layouts/footer/index.jsx
@@ -3,65 +3,66 @@ import Icons from "../../../assets/react-icons";
 const index = memo(() => {
   return (
     <>
-      <div className="footer__container">
-        <div className="container">
-          <div className="row">
-            <div className="d-flex col-sm-6 col-xl-2 col-md-4 flex-column documents">
-              <h5 className="footer_title">Hujjatlar</h5>
-              <ul className="flex-column px-0">
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Umumiy Shartlar</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Offerta arxivi</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Nizom</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Guvohnoma</a></li>
-              </ul>
-            </div>
-            <div className="d-flex col-sm-6 col-xl-2 col-md-4 flex-column service">
-              <h5 className="footer_title">Servis</h5>
-              <ul className="flex-column px-0">
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Do'konlar</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Biz haqimizda</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Hamkorlik uchun</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Qaytarish</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Promokodlar</a></li>
-              </ul>
-            </div>
-            <div className="d-flex col-xl-3 col-sm col-md-4 flex-column items-katalog">
-              <h5 className="footer_title">Mahsulotlar kattaligi</h5>
-              <ul className="flex-column px-0">
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 1</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 1</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 2</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 2</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 3</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 3</a></li>
-              </ul>
-            </div>            
-            <div className="col-sm col offset-md-1 nav-information-service">
-              <h5 className="footer_title">Axborot Xizmati</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">contact@nextstore.uz</a></li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 footer_link">+998 99 999 99 99</a>
-                  <a href="#" className="nav-link p-0 footer_link">+998 99 999 99 99</a>
-                </li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">
-                  Sag’bon 186, Olmazor tumani, Toshkent, O'zbekiston
-                </a></li>
-              </ul>
-              <ul className="nav flex-row information__service_icons">
-                {
-                  Icons.map((icons, index) => (
-                    <li className="nav-item" key={index}>
-                      <a href="" className="nav-link p-0">
-                        <img width={35} src={icons} alt="" />
-                      </a>
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
-            
-            {/* <div className="footer__menu">
+      <footer>
+        <div className="footer__container">
+          <div className="container">
+            <div className="row">
+              <div className="d-flex col-sm-6 col-xl-2 col-md-4 flex-column documents">
+                <h5 className="footer_title">Hujjatlar</h5>
+                <ul className="flex-column px-0">
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Umumiy Shartlar</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Offerta arxivi</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Nizom</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Guvohnoma</a></li>
+                </ul>
+              </div>
+              <div className="d-flex col-sm-6 col-xl-2 col-md-4 flex-column service">
+                <h5 className="footer_title">Servis</h5>
+                <ul className="flex-column px-0">
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Do'konlar</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Biz haqimizda</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Hamkorlik uchun</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Qaytarish</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Promokodlar</a></li>
+                </ul>
+              </div>
+              <div className="d-flex col-xl-3 col-sm col-md-4 flex-column items-katalog">
+                <h5 className="footer_title">Mahsulotlar kattaligi</h5>
+                <ul className="flex-column px-0">
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 1</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 1</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 2</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 2</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 3</a></li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">Katalog 3</a></li>
+                </ul>
+              </div>
+              <div className="col-sm col offset-md-1 nav-information-service">
+                <h5 className="footer_title">Axborot Xizmati</h5>
+                <ul className="nav flex-column">
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">contact@nextstore.uz</a></li>
+                  <li className="nav-item mb-2">
+                    <a href="#" className="nav-link p-0 footer_link">+998 99 999 99 99</a>
+                    <a href="#" className="nav-link p-0 footer_link">+998 99 999 99 99</a>
+                  </li>
+                  <li className="nav-item mb-2"><a href="#" className="nav-link p-0 footer_link">
+                    Sag’bon 186, Olmazor tumani, Toshkent, O'zbekiston
+                  </a></li>
+                </ul>
+                <ul className="nav flex-row information__service_icons">
+                  {
+                    Icons.map((icons, index) => (
+                      <li className="nav-item" key={index}>
+                        <a href="" className="nav-link p-0">
+                          <img width={35} src={icons} alt="" />
+                        </a>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+
+              {/* <div className="footer__menu">
               <ul className="footer__menu_list">
                 <h5 className="footer_title">Hujjatlar</h5>
                 <li className="_menu_item">
@@ -79,15 +80,17 @@ const index = memo(() => {
               </ul>
 
             </div> */}
-          </div>
-          <div className="row">
+            </div>
+            <div className="row">
               <div className="footer__bottom">
                 <span className="float-start">© 2022 Nextstore.com</span>
                 <span className="float-end">Powered by </span>
               </div>
+            </div>
           </div>
         </div>
-      </div>
+      </footer>
+
     </>
   );
 });
